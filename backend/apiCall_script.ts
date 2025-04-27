767f30dd1dcc4715f3bc665abbbddd5bc36c74a9
Victor Kruze Fiori <kruzefiori@hotmail.com>
2025-04-27 14:02:19 -0300

    <Prisma Migration>[Tables]

--- backend/apiCall_script.ts
+++ backend/apiCall_script.ts
@@ -1,26 +1,16 @@
-//make the call to the api
-const urlEndpoint = "https://api.fda.gov/drug/event.json?api_key=8zAOfLqeChggb3QqJuyEEl9ggt4xv0SCYBiuJkqg&limit=10"
-const fetchData = async () => {
-  try {
-    const response = await fetch(urlEndpoint);
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const data = await response.json();
-    console.log("Data fetched successfully:", data);
-    return data;
-  } catch (error) {
-    console.error("Error fetching data:", error);
+const fetchData = async (url: string, apiKey: string) => {
+  console.log("Fetching data from:", url);
+  const finalUrl = new URL(url);
+  finalUrl.searchParams.append("api_key", apiKey);
+
+  console.log("Final URL:", finalUrl.toString());
+
+  const response = await fetch(finalUrl.toString());
+  if (!response.ok) {
+    throw new Error(`Erro na requisição: ${response.status}`);
   }
+
+  return await response.json();
 };
-
-const data = fetchData();
-
-//sponsor name -> nome da empresa
-//products -> produtos dessa empresa -> array de produtos dessa empresa contendo:
-//brand_name -> nome comercial
-//product_number -> número do produto
-//active_ingredients -> array de ingredientes ativos ->  . name -> nome do ingrediente ativo
-//dosage_form -> forma de aplicação
-//route -> via de administração
-//drug name -> nome do medicamento
+const urlEndpoint = "https://api.fda.gov/drug/event.json?limit=10"
+const data = fetchData(urlEndpoint , "8zAOfLqeChggb3QqJuyEEl9ggt4xv0SCYBiuJkqg" )
