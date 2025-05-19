c29b6d9c265e070b09709f28c7e5edb0c13583db
Matheus Araujo <mun1t0@duck.com>
2025-05-19 11:43:37 -0300

    feat: add new migrations

---
+++ backend/prisma/migrations/20250519030320_removed_drug_strenght_field_from_drug/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `drugStrength` on the `Drug` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Drug" DROP COLUMN "drugStrength";
