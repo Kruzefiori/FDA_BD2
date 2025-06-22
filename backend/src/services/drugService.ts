051b11c39cfcd23196612bbc6079d4a8106211f0
Nico Silva <nixon.silva@tech4h.com.br>
2025-06-22 13:55:44 -0300

    feat: add queries with joins

--- backend/src/services/drugService.ts
+++ backend/src/services/drugService.ts
@@ -4,26 +4,76 @@ import prisma from '../prisma/client';
 export const getDrugs = async (table: string, where: any) => {
   console.log(where);
   switch (table) {
-    case 'user':
-      return await prisma.user.findMany({ where });
     case 'shortages':
-      return await prisma.shortages.findMany({ where });
+      return await prisma.shortages.findMany({ 
+        where,
+        include: { Drug: true } 
+      });
     case 'company':
       return await prisma.company.findMany({ where });
     case 'adverseReaction':
-      return await prisma.adverseReaction.findMany({ where });
+      return await prisma.adverseReaction.findMany({ 
+        where,
+        include: { 
+          drugs: {
+            include: {
+              Drug: true
+            }
+          },
+          reportDrugs: {
+            include: {
+              Report: true
+            }
+          }
+        } 
+      });
     case 'report':
-      return await prisma.report.findMany({ where });
+      return await prisma.report.findMany({ 
+        where,
+        include: { 
+          drugs: {
+            include: {
+              Drug: true
+            }
+          },
+          adverseReactions: {
+            include: {
+              AdverseReaction: true
+            }
+          }
+        } 
+      });
     case 'activeIngredient':
-      return await prisma.activeIngredient.findMany({ where });
+      return await prisma.activeIngredient.findMany({ 
+        where,
+        include: { Product: true }
+      });
     case 'product':
       return await prisma.product.findMany({ where });
     case 'relAdverseReactionXDrug':
-      return await prisma.relAdverseReactionXDrug.findMany({ where });
+      return await prisma.relAdverseReactionXDrug.findMany({ 
+        where,
+        include: { 
+          AdverseReaction: true,
+          Drug: true 
+        } 
+      });
     case 'relAdverseReactionXReport':
-      return await prisma.relAdverseReactionXReport.findMany({ where });
+      return await prisma.relAdverseReactionXReport.findMany({ 
+        where,
+        include: { 
+          AdverseReaction: true,
+          Report: true 
+        } 
+      });
     case 'relReportXDrug':
-      return await prisma.relReportXDrug.findMany({ where });
+      return await prisma.relReportXDrug.findMany({ 
+        where,
+        include: { 
+          Report: true,
+          Drug: true 
+        } 
+      });
     case 'drug':
       return await prisma.drug.findMany({ where });
     default:
